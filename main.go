de98b82c5f8bd4d0e6be94e1e06da24ca6e082ba
Hossein Esmail <hosteam01@gmail.com>
2024-09-13 21:46:42 +0330

    Just a demo; NewItem(), IntractiveMenu()

--- main.go
+++ main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
-    "errors"
+	"os"
 
-    "github.com/charmbracelet/huh"
-    _ "gopkg.in/yaml.v3"
+	"github.com/charmbracelet/huh"
+    // _ "github.com/charmbracelet/lipgloss"
+	_ "gopkg.in/yaml.v3"
 )
 
-// add new item {{{
+var (
+    argc int = len(os.Args)
+    argv []string = os.Args
+    YamlPath string = os.Getenv("HOME")+"/.local/share/golem"
+)
+
+// add new item 
 func NewItem(filepath string) {
     var title        string
     var descriptiion string
     var check        bool
+
+    tmp := DoubleCheck("testing DoubleCheck function")
+    fmt.Println(tmp)
 
     form := huh.NewForm(
         huh.NewGroup(
@@ -33,15 +44,17 @@ func NewItem(filepath string) {
             Value(&descriptiion),
 
             huh.NewConfirm().
-            Title("Add the item to the todo list?").
+            Title("Add it?").
             Value(&check),
         ),
-    ).WithTheme(huh.ThemeBase())
+    ).WithTheme(huh.ThemeCharm())
 
     err := form.Run()
     if err != nil {
         log.Fatal(err)
     }
+
+    // check = DoubleCheck("Are you sure?")
 
     fmt.Println(title)
     fmt.Println(descriptiion)
@@ -52,19 +65,74 @@ func NewItem(filepath string) {
     // }
 
 }
-// }}}
 
-// intractive menu {{{
-func IntractiveMenu(filepath string) {
+// double check
+func DoubleCheck(check_message string) bool {
+    var check        bool
+    form := huh.NewForm(
+        huh.NewGroup(
+            huh.NewConfirm().
+            Title(check_message).
+            Value(&check),
+        ),
+    ).WithTheme(huh.ThemeCharm())
+
+    err := form.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return check
 }
-// }}}
 
-// dispaly items {{{
+// intractive menu 
+func IntractiveMenu(filepath string) string {
+    var choice string
+    form := huh.NewForm(
+        huh.NewGroup(
+            huh.NewSelect[string]().
+                Title("Golem TODO app, What's your business here?").
+                Options(
+                    huh.NewOption("I wanna add a new task", "add"),
+                    huh.NewOption("Give me a summary", "show"),
+                    huh.NewOption("I need to change smth", "edit"),
+                    huh.NewOption("Some works are done, don't need them any more", "delete"),
+                ).
+            Value(&choice),
+        ),
+    ).WithTheme(huh.ThemeCharm())
+
+    err := form.Run()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return choice
+}
+
+// display items 
 func DisplayItems(filepath string) {
 }
-// }}}
 
+
+// main func
 func main() {
-    NewItem("test")
+    // fmt.Println(argc)
+    // fmt.Println(argv)
+    if argc == 1 {
+        switch menu := IntractiveMenu("/tmp/test.yml"); menu {
+        case "add":
+            NewItem(YamlPath+"/db.yml")
+        case "see":
+            // see
+        case "edit":
+            // edit
+        case "delete":
+            // delete
+        default:
+            log.Fatalln("Unkown behavior")
+        }
+        // NewItem("test")
+    }
 }
 
